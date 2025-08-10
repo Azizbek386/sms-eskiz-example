37d4a7b43eb753abe866e4f4ea4d3acd12518bcb
Azizbek <azizbekbazarbaev0606@gmail.com>
2025-08-10 11:40:25 +0500

    Projects routes writed

---
+++ routes/api.php
@@ -0,0 +1,16 @@
+<?php
+
+use App\Http\Controllers\AuthController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::middleware( 'setLocale')->group(function(){
+    Route::middleware('auth:sanctum')->group(function(){
+        Route::get('/user', [AuthController::class, 'getUser']);
+        Route::get('/logout', [AuthController::class, 'logout']);
+    });
+
+    Route::post('/verify-phone', [AuthController::class, 'verifyPhone']);
+    Route::post('/register', [AuthController::class, 'register']);
+    Route::post('/login', [AuthController::class, 'login']);
+});
